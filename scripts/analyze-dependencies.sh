e1444acf22cc5bbc8bdd0543aaf79d3da4c9fb28
William Griffiths <604469+wgriffiths@users.noreply.github.com>
2024-03-27 16:03:14 +0000

    Changes so action works on Windows

--- scripts/analyze-dependencies.sh
+++ scripts/analyze-dependencies.sh
@@ -4,7 +4,6 @@ DEPENDENCIES=$(jq -r ' (.devDependencies // {}, .dependencies // {} ) | keys[]' 
 
 if grep -q "@sknups-internal/" <<< "$DEPENDENCIES" ; then
   echo "authenticate=true" >> "$GITHUB_OUTPUT"
-  echo "this project depends on package(s) scoped '@sknups-internal'" >> /dev/stdout
 else
   echo "authenticate=false" >> "$GITHUB_OUTPUT"
 fi
